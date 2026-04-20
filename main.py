d52a9d8a4ae24c8b6a85af552b81ffc2adc420ff
Dilshod <ddilshod2197@gmail.com>
2026-04-20 23:47:16 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,27 @@
+def sozlar_alfavitga_saralash(sozlari):
+    sozlari = sozlari.lower().split()
+    sozlari.sort()
+    return ' '.join(sozlari)
+
+sozlar = input("So'zlarni kiriting: ")
+print(sozlar_alfavitga_saralash(sozlar))
+```
+
+```python
+def sozlar_alfavitga_saralash(sozlari):
+    sozlari = sozlari.lower().split()
+    sozlari.sort()
+    return ' '.join(sozlari)
+
+sozlar = input("So'zlarni kiriting: ")
+print(sozlar_alfavitga_saralash(sozlar))
+```
+
+```python
+def sozlar_alfavitga_saralash(sozlari):
+    sozlari = sozlari.lower().split()
+    sozlari.sort()
+    return ' '.join(sozlari)
+
+sozlar = input("So'zlarni kiriting: ")
+print(sozlar_alfavitga_saralash(sozlar))
